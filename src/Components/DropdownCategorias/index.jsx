9a6a4560d14d8ec51882f817143da046b78f5683
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2023-06-16 14:05:07 -0300

    enhancing language change process

--- src/Components/DropdownCategorias/index.jsx
+++ src/Components/DropdownCategorias/index.jsx
@@ -1,5 +1,5 @@
 import { Dropdown } from "react-bootstrap";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { LanguageContext } from "../../Providers/LanguageProvider";
 
 function DropdownCategorias() {
@@ -18,6 +18,18 @@ function DropdownCategorias() {
         const event = new CustomEvent('categoriaChange',{ detail: eventKey });
         document.dispatchEvent(event);
     }
+    useEffect(() => {
+        if(language !== 'br')
+        {
+            const index = categorias.indexOf(categoria)
+            setCategoria(categoriasEn[index]);
+        }
+        else
+        {
+            const index = categoriasEn.indexOf(categoria)
+            setCategoria(categorias[index]);
+        }
+    }, [language])
     return ( 
     <>
     <Dropdown onSelect={handleSelect}>
