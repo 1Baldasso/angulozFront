cf46e443b00aa61e155647db5ac5303cbbdfa325
Alexandre <86206091+Alexandre10y@users.noreply.github.com>
2023-06-26 21:51:34 -0300

    Adicionando Legado
    Textos convertidos para o ingles + imagens com descrições

--- src/Documents/LegadoData.js
+++ src/Documents/LegadoData.js
@@ -4,8 +4,40 @@ const LegadoData = {
         br: "Legado"
     },
     descricao:{
-        br: "Nosso desafio é fazer projeto que serão vibrantes daqui a décadas. O designer conecta sentimentos, a emoção é uma função... E o principal objetivo é criar ideias e fazer elas saírem pelo mundo. Parte do nosso trabalho é criar projetos que tenham impacto significativo no espaço, onde cada projeto será revolucionário á sua maneira. Um espírito humanístico é algo importante que incorporamos no que construímos, identificamos e exploramos as possibilidades... dando forma ao mundo em que nós gostaríamos de viver no futuro. Se você realmente inspira esperança, você capacita as pessoas a entender que podemos fazer a diferença no mundo. Esse sentimento é expresso em nossos projetos... é emocionante e são esses resultados que nos impulsiona a continuar produzindo uma arquitetura impactante, inclusiva e transformadora...",
-        en:"Our challenge is to create projects that will be vibrant for decades to come. The designer connects emotions, and emotion is a function... And the main objective is to generate ideas and make them come to life in the world. Part of our work is to create projects that have a significant impact on space, where each project will be revolutionary in its own way. A humanistic spirit is something important that we incorporate into what we build, identify, and explore the possibilities... shaping the world in which we would like to live in the future. If you truly inspire hope, you empower people to understand that we can make a difference in the world. This feeling is expressed in our projects... it's exciting, and it is these results that drive us to continue producing impactful, inclusive, and transformative architecture."
-    }
+        br: "Nosso desafio é fazer projeto que serão vibrantes daqui a décadas.",
+        en:"Our challenge is to create projects that will be vibrant for decades to come."
+    },
+    descricao2:{
+        br: "O papel da arquitetura é conectar emoção, função e conceito, parte do nosso trabalho é criar projetos que tenham impacto significativo no espaço. Buscamos incorporar ideias inovadoras, soluções criativas que exploram as possibilidades, identificamos e damos forma ao mundo, em que nós gostaríamos de viver no futuro.",
+        en:"The role of architecture is to connect emotion, function, and concept. Part of our work is to create projects that have a significant impact on space. We strive to incorporate innovative ideas and creative solutions that explore possibilities. We identify and shape the world in which we would like to live in the future."
+    },
+    descricao3:{
+        br:"Esse sentimento é expresso em nossos projetos, são esses resultados que nos impulsiona a continuar produzindo uma arquitetura impactante, inclusiva e transformadora. ",
+        en:"This sentiment is expressed in our projects, and it is these results that drive us to continue producing impactful, inclusive, and transformative architecture."
+    },
+    descricao4:{
+        br:"E o principal objetivo é criar ideias e fazer elas saírem pelo mundo!",
+        en:"And the main objective is to create ideas and let them spread throughout the world!"
+    },
+    descricao5:{
+        br:"Idealizado por uma Arquiteta e uma Engenheira, o escritório foi criado para atuar nas mais diversas escalas. Em cada projeto que elaboramos colocamos nossa identidade, porque entendemos que tudo depende do ÂNGULO, e mudar o ponto de vista transforma os resultados. ",
+        en:"Founded by an architect and an engineer, the firm was created to work at various scales. In every project we develop, we infuse it with our own identity, because we understand that everything depends on the ANGLE, and changing the point of view transforms the outcomes."
+    },
+    descricao6:{
+        br:"“Arquitetura é minha paixão! A arquitetura me inspira a alcançar novos patamares, materializar ideias e proporciona um sentimento proeminente, que me dá um senso de direção e significado. Acredito que tudo é um processo, e muitas vezes basta realizar o básico bem feito, e colocar uma pitada de autenticidade.’’ ",
+        en:"\"Architecture is my passion! It inspires me to reach new heights, materialize ideas, and evokes a prominent feeling that gives me a sense of direction and meaning. I believe that everything is a process, and often, all it takes is to do the basics well and add a touch of authenticity.\""
+    },
+    descricao7:{
+        br:"Mariza da Silveira - Arquiteta e Urbanista",
+        en:"Maria D'Angelo - Architect and Urban Planner"
+    },
+    descricao8:{
+        br:"\“A engenharia transforma, cria e executa cada detalhe. Ser um engenheiro é projetar o futuro, é solucionar problemas, é ser designado a fazer parte de um sonho. Tudo vai além de linhas projetadas, mas com elas nós podemos nos imaginar dentro dele, e tudo pode ser criado.\” ",
+        en:"\"Engineering transforms, creates, and executes every detail. Being an engineer means designing the future, solving problems, and being entrusted to be part of a dream. It goes beyond projected lines; it allows us to envision ourselves within it, and everything can be created.\""
+    },
+    descricao9:{
+        br:"Brenda Giordani - Engenheira Eletricista",
+        en:"Brenda Giordani - Electrical Engineer"
+    },
 }
 export default LegadoData;
